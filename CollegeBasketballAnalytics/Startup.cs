a0f22d4d4f1ded11b2339f5c266d94442afd9167
Max Hahn <max.vhahn@gmail.com>
2022-03-19 22:18:02 -0400

    Add project files.

---
+++ CollegeBasketballAnalytics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CollegeBasketballAnalytics.Startup))]
+namespace CollegeBasketballAnalytics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
